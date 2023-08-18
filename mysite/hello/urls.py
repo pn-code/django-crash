46eb1edf60e38337d0ebf3952ba871f3d1b65f19
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-18 16:33:27 -0700

    Added guest app to hello

--- mysite/hello/urls.py
+++ mysite/hello/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('guest', views.guest, name='guest'),
 ]
--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+def guest(request):
+    return HttpResponse("Hello, guest!")
