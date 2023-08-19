eff2dc492e0db13f53328239af18ea947fa58cfd
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-19 15:15:05 -0700

    Updated views/greet to render html template

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -9,4 +9,6 @@ def guest(request):
     return HttpResponse("Hello, guest!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
