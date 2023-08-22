a37e0dc3a0bf13690002edad063f0b99e3a40487
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-22 13:22:25 -0700

    Added form validation on client and server

--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -3,11 +3,22 @@ from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
 
-class newTaskForm(forms.Form):
+
+class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task", min_length=1, max_length=100)
+
 
 def index(request):
     return render(request, 'tasks/index.html', {'tasks': tasks})
 
+
 def add(request):
-    return render(request, 'tasks/add.html', {'form': newTaskForm})
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, 'tasks/add.html', {'form': form})
+
+    return render(request, 'tasks/add.html', {'form': NewTaskForm()})
