b95d56765645720eb7f3696af42a548169844e21
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-22 13:13:23 -0700

    Replaced hard-coded html form with Django form

--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -1,10 +1,13 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
 
+class newTaskForm(forms.Form):
+    task = forms.CharField(label="New Task", min_length=1, max_length=100)
 
 def index(request):
     return render(request, 'tasks/index.html', {'tasks': tasks})
 
 def add(request):
-    return render(request, 'tasks/add.html')
+    return render(request, 'tasks/add.html', {'form': newTaskForm})
